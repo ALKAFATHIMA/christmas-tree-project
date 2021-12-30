cec2ddffde7b0bab7d5c50abe4143ca266983b77
ALKAFATHIMA <96837643+ALKAFATHIMA@users.noreply.github.com>
2021-12-30 12:30:23 +0530

    Add files via upload

---
+++ X-Mas tree code.py
@@ -0,0 +1,6 @@
+print("ALKA FATHIMA.V.B S1 CSE")
+print("     MERRY CHRISTMAS ")
+Height =12
+for i in range(Height ):
+      print(( ' '*(Height-i))+("*"*((2*i)+1)))
+      print((' '*Height )+'|')
